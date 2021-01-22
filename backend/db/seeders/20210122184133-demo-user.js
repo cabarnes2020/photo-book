773b380f1a7e3f46a7cdfb5d6d73edf58834dd06
Christion Barnes <cabarnes0497@gmail.com>
2021-01-22 15:41:20 -0600

    create login route

---
+++ backend/db/seeders/20210122184133-demo-user.js
@@ -0,0 +1,50 @@
+'use strict';
+const faker = require('faker');
+const bcrypt = require('bcryptjs');
+const { query } = require("express");
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Users', [
+      {
+        email: 'demo@user.io',
+        username: 'Demo-lition',
+        hashedPassword: bcrypt.hashSync('password'),
+      },
+      {
+        email: faker.internet.email(),
+        username: 'FakeUser1',
+        hashedPassword: bcrypt.hashSync(faker.internet.password()),
+      },
+      {
+        email: faker.internet.email(),
+        username: 'FakeUser2',
+        hashedPassword: bcrypt.hashSync(faker.internet.password()),
+      },
+    ], {})
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete('Users', {
+      username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
+    }, {});
+  }
+};
