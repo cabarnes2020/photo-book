4975c217a5572cb090b5fffb7d857b5a369f9dbe
Christion Barnes <cabarnes0497@gmail.com>
2021-01-26 21:55:58 -0600

    created dashboard component

---
+++ frontend/src/components/DashboardPage/index.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
+
+
+const DashboardPage = () => {
+
+
+
+
+    return (
+        <div className='dashboard-page'> 
+            
+            <h1>Welcome to PhotoBook</h1>
+
+        </div>
+    )
+
+
+
+
+
+
+
+
+
+
+
+
+} 
+
+export default DashboardPage;
